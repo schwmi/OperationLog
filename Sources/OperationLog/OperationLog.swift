f0b1f8660afcf49d6a6b0f4faf680ddcc7ebfd14
Michael Schwarz <michael_schwarz@me.com>
2020-09-17 09:07:59 +0200

    Implement append and merge

--- Sources/OperationLog/OperationLog.swift
+++ Sources/OperationLog/OperationLog.swift
@@ -4,7 +4,25 @@ import VectorClock
 
 struct OperationLog<ActorID: Comparable & Hashable & Codable> {
 
+    struct OperationContainer: Equatable, Hashable {
+
+        static func == (lhs: OperationLog<ActorID>.OperationContainer, rhs: OperationLog<ActorID>.OperationContainer) -> Bool {
+            return lhs.clock == rhs.clock
+        }
+
+        func hash(into hasher: inout Hasher) {
+            hasher.combine(self.clock)
+        }
+
+        let clock: VectorClock<ActorID>
+        let operation: Operation
+    }
+
     private let actorID: ActorID
+    private var operations: [OperationContainer] = []
+    private var currentClock: VectorClock<ActorID> {
+        return operations.last?.clock ?? VectorClock(actorID: self.actorID)
+    }
 
     // MARK: - Lifecycle
 
@@ -12,7 +30,16 @@ struct OperationLog<ActorID: Comparable & Hashable & Codable> {
         self.actorID = actorID
     }
 
-    func append(_ operation: Operation) {
+    // MARK: - OperationLog
+
+    mutating func append(_ operation: Operation) {
+        self.operations.append(.init(clock: self.currentClock.incrementing(self.actorID),
+                                     operation: operation))
+    }
+
+    mutating func merge(_ operationLog: OperationLog) {
+        let allOperations = Set(operationLog.operations + self.operations)
+        self.operations = allOperations.sorted(by: { $0.clock < $1.clock })
     }
 }
 
@@ -20,6 +47,7 @@ protocol Operation {
     var description: String? { get }
     func apply(to snapshot: Snapshot) -> Snapshot
     func serialize() -> Data
+    func reverted() -> Operation
 }
 
 protocol Snapshot {
